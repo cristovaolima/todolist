3dfa1af61febd8c1d46c5c058a905c662e72fb44
Cristovão Lima <37450799+cristovaolima@users.noreply.github.com>
2021-03-13 21:54:12 -0300

    Excluir tarefa

--- src/Componets/Realizadas/index.js
+++ src/Componets/Realizadas/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Container, Text } from './styles'
+import { Container, Text, CenterViw, Botao, TextBotao, CenterViwBranco, TextBotaoChecck } from './styles'
 
-export default function Realizadas() {
+export default function Realizadas({ data, excluir }) {
  return (
-   <Container>
-   <Text>Atividades</Text>
-</Container>
+    <Container>
+      {data.status === '1' ? (
+        <CenterViw>            
+        <Text>
+        <TextBotaoChecck>✓ </TextBotaoChecck>
+          {data.titulo}</Text>
+        <Botao onPress={() => excluir(data)}>
+            <TextBotao>✕</TextBotao>
+        </Botao>
+      </CenterViw>
+      ) : (<CenterViwBranco></CenterViwBranco>)
+
+      }
+    </Container>   
   );
 }
