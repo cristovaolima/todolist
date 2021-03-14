db26079e38e6b46ce67946db9dc8824d9dbeb019
Cristovão Lima <37450799+cristovaolima@users.noreply.github.com>
2021-03-13 21:53:40 -0300

    Salvar, editar, excluir e alterar status da tarefa

--- src/Componets/Atividades/index.js
+++ src/Componets/Atividades/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Container, Text, CenterViw, Botao, TextBotao } from './styles'
+import { Container, Text, CenterViw, Botao, TextBotao, CenterViwBranco, BotaoChecck, TextBotaoChecck } from './styles'
 
-export default function Atividades({ data }) {
+export default function Atividades({ data, editar, excluir, status }) {
  return (
     <Container>
-      <CenterViw>            
-        <Text>Atividades</Text>
-        <Botao>
-            <TextBotao>Editar</TextBotao>
+      {data.status === '0' ? (
+      <CenterViw>
+        <Text>
+        <BotaoChecck onPress={() => status(data)} >
+          <TextBotaoChecck>❑ </TextBotaoChecck>
+        </BotaoChecck>
+          {data.titulo}
+        </Text>            
+        <Botao onPress={() => editar(data)}>
+          <TextBotao>✎</TextBotao>
         </Botao>
-        <Botao>
-            <TextBotao>Excluir</TextBotao>
+        <Botao onPress={() => excluir(data)}>
+          <TextBotao>✕</TextBotao>
         </Botao>
       </CenterViw>
+      ): (<CenterViwBranco></CenterViwBranco>)}
     </Container>   
   );
 }
